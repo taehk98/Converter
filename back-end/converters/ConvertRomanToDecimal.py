3a446d003dda69d6accd407ed8e5c595ed459a71
Taehoon <kthsh98@gmail.com>
2024-03-30 01:21:45 -0600

    adds converters

---
+++ back-end/converters/ConvertRomanToDecimal.py
@@ -0,0 +1,18 @@
+import RomanNumeralConverter
+
+class ConvertRomanToDecimal(RomanNumeralConverter):
+    # override convert_to_decimal from RomanNumeralConverter
+    def convert_to_decimal(self, input_roman):
+        preceed_value = 0
+        decimal = 0
+        for curr_numeral in reversed(input_roman):
+            curr_value = self.roman_numeral[curr_numeral]
+            if preceed_value < curr_value:
+                if preceed_value < curr_value/10:
+                    raise ValueError("This is an invalid input")
+                decimal += curr_value
+            else:
+                decimal -= curr_value
+            preceed_value = curr_value
+        return decimal
+    
---
+++ back-end/converters/RomanNumeralConverter.py
@@ -0,0 +1,17 @@
+class RomanNumeralConverter:
+    def __init__(self):
+        self.roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+    
+    def convert_to_decimal(self, roman):
+        raise SyntaxError("This is not supported")
+    
+    def convert_to_roman_numeral(self, decimal):
+        raise SyntaxError("This is not supported")
